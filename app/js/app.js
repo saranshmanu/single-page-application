9c4ccac172dd24d08780c21665f2f09ac11855d8
Saransh Mittal <saransh.mittal1998@gmail.com>
2021-03-15 00:42:13 +0530

    Updates the application with backend dummy values

--- app/js/app.js
+++ app/js/app.js
@@ -22,17 +22,36 @@ window.addEventListener('load', () => {
     return accum;
   });
 
+  Handlebars.registerHelper('ifEquals', function(a, b, options) {
+    if (a === b) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+
   router.add('/product', async () => {
-    let html = productTemplate();
-    el.html(html);
+    try {
+      const product = await api.get('/product' + window.location.search);
+      let html = productTemplate(product.data);
+      el.html(html);
+    } catch(err) {
+      console.error(err);
+    }
   });
 
   router.add('/', async () => {
     let html = shopTemplate();
     el.html(html);
+    try {
+      const products = await api.get('/products');
+      let html = shopTemplate({products: products.data});
+      el.html(html);
+    } catch(err) {
+      console.error(err);
+    }
   });
 
-  router.navigateTo(window.location.pathname);
+  router.navigateTo(window.location.pathname + window.location.search);
 
   function navigate(event) {
     event.preventDefault();
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
 const build = require('./app/build');
+const products = require('./app/data/products.json');
 
 const app = express();
 const port = 3000;
 
+// Configure Logger
+app.use(morgan('tiny'));
+
 // Set public folder as root
 app.use(express.static('public'));
 
-// Configure Logger
-app.use(morgan('tiny'));
+app.get('/api/products', function(req, res) {
+  res.send(products);
+});
+
+app.get('/api/product', function(req, res) {
+  const uuid = req.query.id;
+  var response = {};
+  products.map((product) => {
+    if (product.id.toString() === uuid) {
+      response = product;
+    }
+  });
+  res.send(response);
+});
 
 // Provide access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Redirect all traffic to index.html
-app.use((_, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.use((_, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
 app.listen(port, () => {
   build();
